7a283e959b79fba2eefc59b1d759be977d306b17
Sergey Jakobsen <46766403+sergiosja@users.noreply.github.com>
2021-11-14 13:57:05 +0100

    Cleaner delimitersoup

--- solutions/delimitersoup.py
+++ solutions/delimitersoup.py
@@ -1,18 +1,19 @@
 def main():
     cs = input()
     stack = []
-    all = [ "{", "[", "(", ")", "]", "}" ]
+    dem = [ "{", "[", "(", ")", "]", "}" ]
     opp = { "}": "{", ")": "(", "]": "[" }
 
     for i in range(len(cs)):
-        if cs[i] in all[:3]:
+        if cs[i] in dem[:3]:
             stack.append(cs[i])
-        else:
-            if cs[i] in all[3:]:
-                if not stack or stack[-1] != opp[cs[i]]:
-                    print(cs[i], i)
-                    return
-                stack.pop()
+            continue
+            
+        if cs[i] in dem[3:]:
+            if not stack or stack[-1] != opp[cs[i]]:
+                print(cs[i], i)
+                return
+            stack.pop()
 
     print("ok so far")
 
